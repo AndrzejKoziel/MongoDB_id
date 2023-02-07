53e3999d588fe512692ec371b32c9304fe7ead93
Andrzej Koziel <Andrzej.Koziel@hyland.com>
2023-02-07 16:49:34 +0100

    Add project files.

---
+++ MongoDB_id/Model.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MongoDB_id
+{
+    public class Model
+    {
+        [BsonId]
+        [BsonRequired]
+        [BsonElement("id")]
+        [BsonGuidRepresentation(GuidRepresentation.Standard)]
+        public Guid Id { get; set; }
+
+    }
+}
---
+++ MongoDB_id/Program.cs
@@ -0,0 +1,55 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+using MongoDB_id;
+
+BsonSerializer.RegisterSerializer(new GuidSerializer(GuidRepresentation.Standard));
+
+var mongoUrl = new MongoUrl("mongodb://admin:admin@localhost:27017/?directConnection=true");
+var dbName = "TestDB";
+var colletionName = "TestCollection";
+
+
+
+var mongoClient = new MongoClient(mongoUrl);
+var mongoDB = mongoClient.GetDatabase(dbName);
+var mongoCollection = mongoDB.GetCollection<Model>(colletionName);
+
+var document = new Model
+{
+    Id = Guid.NewGuid()
+
+};
+
+mongoCollection.InsertOne(document);
+
+var res1 = mongoCollection.FindSync(x => x.Id == document.Id).ToList();
+var res2 = mongoCollection.Find(x => x.Id == document.Id).ToList();
+
+var filter = "{ \"_id\" : UUID(\"" +document.Id.ToString() + "\") }";
+
+var res3 = mongoCollection.Find(filter).ToList();
+
+if (res1.Count <= 0)
+    Console.WriteLine("Res1 - no documents");
+else
+    Console.WriteLine($"Res1 - {res2.Count} documents");
+
+if (res2.Count <= 0)
+    Console.WriteLine("Res2 - no documents");
+else
+    Console.WriteLine($"Res2 - {res1.Count} documents");
+
+if (res3.Count <= 0)
+    Console.WriteLine("Res3 - no documents");
+else
+    Console.WriteLine($"Res3 - {res2.Count} documents");
+
+Console.ReadLine();
+
+
+
+
