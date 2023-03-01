dffbc3c99b396ac1692521244267b331f0b48b73
Andrzej Koziel <Andrzej.Koziel@hyland.com>
2023-03-01 17:03:08 +0100

    TEST signed commit

--- MongoDB_id/Program.cs
+++ MongoDB_id/Program.cs
@@ -5,14 +5,17 @@ using MongoDB.Driver;
 using MongoDB.Bson.Serialization;
 using MongoDB.Bson.Serialization.Serializers;
 using MongoDB_id;
+using System.Text.Json.Serialization;
+using System.Text.Json;
 
 BsonSerializer.RegisterSerializer(new GuidSerializer(GuidRepresentation.Standard));
 
+
+BsonDefaults.GuidRepresentationMode = GuidRepresentationMode.V3;
+
 var mongoUrl = new MongoUrl("mongodb://admin:admin@localhost:27017/?directConnection=true");
 var dbName = "TestDB";
-var colletionName = "TestCollection";
-
-
+var colletionName = "TestCollection
 
 var mongoClient = new MongoClient(mongoUrl);
 var mongoDB = mongoClient.GetDatabase(dbName);
@@ -47,6 +50,18 @@ if (res3.Count <= 0)
     Console.WriteLine("Res3 - no documents");
 else
     Console.WriteLine($"Res3 - {res2.Count} documents");
+
+
+JsonSerializerOptions serializerOptions = new()
+{
+    PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+};
+
+serializerOptions.ConfigureForNodaTime(DateTimeZoneProviders.Tzdb);
+serializerOptions.Converters.Add(new JsonStringEnumConverter(JsonNamingPolicy.CamelCase, true));
+builder.Services.AddSingleton(serializerOptions);
+        }
+
 
 Console.ReadLine();
 
